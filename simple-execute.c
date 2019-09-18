40d07a63356abdb9f9a69a89a8568a756290f2c1
Yu Chi To <yuchitoto@gmail.com>
2019-09-18 16:46:01 +0800

    declaring func needed

--- simple-execute.c
+++ simple-execute.c
@@ -5,25 +5,27 @@
 #include <errno.h>
 #include <unistd.h>
 
+//need another func to pipe any command after | to terminal
+
 int shell_execute(char ** args, int argc)
 {
 	int child_pid, wait_return, status;
 
 	if ( strcmp(args[0], "EXIT") == 0 )
-		return -1; 
-	
+		return -1;
+
 	if( (child_pid = fork()) < 0 ){
 		printf("fork() error \n");
 	}else if (child_pid == 0 ){
-		if ( execvp(args[0], args) < 0){ 
+		if ( execvp(args[0], args) < 0){
 			printf("execvp() error \n");
 			exit(-1);
 		}
 	}else{
 		if ( (wait_return = wait(&status) ) < 0 )
-			printf("wait() error \n"); 
+			printf("wait() error \n");
 	}
-			
+
 	return 0;
 
 }
