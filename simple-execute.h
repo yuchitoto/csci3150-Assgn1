31bf7b4a587ad40faba9e835c2b0c8bf9919cc9d
yuchitoto <yuchitoto@gmail.com>
2019-09-26 12:12:54 +0800

    remove unused header after merging code

--- simple-execute.h
+++
@@ -1,8 +0,0 @@
-#ifndef SIMPLE_EXECUTE_H_
-#define SIMPLE_EXECUTE_H_
-
-void shell_execute1(char **args, int argc);
-void shell_execute2(char **args, int argc);
-int shell_execute(char **args, int argc);
-
-#endif //SIMPLE-EXECUTE_H_
