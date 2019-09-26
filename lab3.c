4a696e8672c9ff5d784481edccc4c979b83d403e
Yu Chi To <yuchitoto@gmail.com>
2019-09-26 11:45:45 +0800

    add Debug statement

--- lab3.c
+++ lab3.c
@@ -9,6 +9,9 @@ int main(void)
 	char **args;
 	char *line = "ls -l | grep D | wc -l \0";
 	int argc = get_line_args(line,args);
+	printf("%d\n",argc);
+	for(int i=0;i<argc;i++)
+	printf("%s\n",(args[i]!=NULL)?args[i]:"someNULL");
 	int status = shell_execute(args,argc);
 	return 0;
 }
