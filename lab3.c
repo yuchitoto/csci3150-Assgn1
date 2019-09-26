696fac18e523badaac9afef287030be7e3738a3a
yuchitoto <yuchitoto@gmail.com>
2019-09-26 12:00:05 +0800

    fix

--- lab3.c
+++ lab3.c
@@ -5,8 +5,8 @@
 int main(void)
 {
 	char *args[9] = {"ls","-l","|","grep","D","|","wc","-l",NULL};
-	for(int i=0;i<9;i++)
-	//printf("%s\n",(args[i]!=NULL)?args[i]:"someNULL");
+	/*for(int i=0;i<9;i++)
+	printf("%s\n",(args[i]!=NULL)?args[i]:"someNULL");*/
 	int status = shell_execute(args,9);
 	return 0;
 }
